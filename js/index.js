f566fa3c520f39b86dc99328ddb087d91c3eb410
Max Bye <max.bye@bp.com>
2024-06-22 12:47:57 +0100

    logo wobble

--- js/index.js
+++ js/index.js
@@ -7,6 +7,6 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     imgElement.addEventListener('mouseleave', function () {
-        imgElement.setAttribute('src', './img/FRAZZLED_LOGO_no_background.gif');
+        imgElement.setAttribute('src', 'https://cdn.builder.io/api/v1/image/assets/TEMP/545ed2e3d54f95739fd120be7ed28730be7dc6f79604623f42ba3920a5109209?apiKey=f2493124652b497ab521fbc809f44def&');
     });
 });
